ca68b5bbe0e8bf2045b5078f9af9adce8b278731
MdTasib <ohidulalam442@gmail.com>
2022-07-20 16:08:00 +0600

    error & exception handling

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(errorHandler);
 
 // Routes
 app.use("/", require("./routes/userRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Middlewares
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log("Server is running on", PORT));
